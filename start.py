1270f5167b34daad17f04c39c425e28dc5936ec4
franlinGaming5475 <84828121+franlinGaming5475@users.noreply.github.com>
2021-05-26 12:42:08 +0530

    start.py

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
-from os import system # chala ja bhosdike madarchod
+from os import system # chala ja bhai
 system ("git clone https://GitHub.com/franklinGaming5475/HEROKU && cd ULTRA-X && python3 -m ULTRA")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @UltraXchaT")
